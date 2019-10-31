7c13dc13fd60adc4c17a34a04c354e58e86304a4
mmcassa <47122234+mmcassa@users.noreply.github.com>
2019-10-31 10:45:40 -0400

    File Declarations

---
+++ graph.c
@@ -0,0 +1,18 @@
+#include "graph.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <mpi.h>
+
+
+void read_graph(char *file_name, int *n, int ***A) {
+
+}
+
+void write_graph(char *file_name, int n, int **A) {
+
+}
+
+void print_graph(int *n, int ***A) {
+
+}
---
+++ graph.h
@@ -0,0 +1,8 @@
+#ifndef _graph_
+#define _graph_
+
+void read_graph(char *file_name, int *n, int ***A);
+void write_graph(char *file_name, int n, int **A);
+void print_graph(int *n, int ***A);
+
+#endif 
